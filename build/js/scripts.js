dd3bd9bf019917d2e6e1b8edb422d1ac6bf8d901
Yakup <yakupjoraev@gmail.com>
2024-07-23 17:51:14 +0300

    added new pages

---
+++ build/js/scripts.js
@@ -0,0 +1,112 @@
+// Custom Scripts
+// Custom scripts
+// Мобильное меню бургер
+function burgerMenu() {
+  const container = document.querySelector('.menu');
+
+  if (!container) {
+    return null
+  }
+  const burger = document.querySelector('.burger')
+  const menu = document.querySelector('.menu')
+  const body = document.querySelector('body')
+  burger.addEventListener('click', () => {
+    if (!menu.classList.contains('active')) {
+      menu.classList.add('active')
+      burger.classList.add('active-burger')
+      body.classList.add('locked')
+    } else {
+      menu.classList.remove('active')
+      burger.classList.remove('active-burger')
+      body.classList.remove('locked')
+    }
+  })
+  //снять классы при клике на элементы меню
+  const menuItems = document.querySelectorAll('.menu__item')
+
+  menuItems.forEach(item => {
+    item.addEventListener('click', function () {
+      menu.classList.remove('active')
+      burger.classList.remove('active-burger')
+      body.classList.remove('locked')
+    })
+  });
+
+  // Вот тут мы ставим брейкпоинт навбара
+  window.addEventListener('resize', () => {
+    if (window.innerWidth > 991.98) {
+      menu.classList.remove('active')
+      burger.classList.remove('active-burger')
+      body.classList.remove('locked')
+    }
+  })
+}
+burgerMenu()
+
+
+// Вызываем эту функцию, если нам нужно зафиксировать меню при скролле.
+function fixedNav() {
+  const nav = document.querySelector('nav')
+
+  // тут указываем в пикселях, сколько нужно проскроллить что бы наше меню стало фиксированным
+  const breakpoint = 1
+  if (window.scrollY >= breakpoint) {
+    nav.classList.add('fixed__nav')
+  } else {
+    nav.classList.remove('fixed__nav')
+  }
+}
+window.addEventListener('scroll', fixedNav)
+
+
+//плавный скролл
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+
+    const blockID = anchor.getAttribute('href').substr(1)
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+};
+
+
+// Аккордеон
+const accordionItems = document.querySelectorAll('[data-accordion-item]');
+let openAccordion = null; // переменная для хранения ссылки на открытый аккордеон
+
+function toggleAccordion() {
+  if (openAccordion && openAccordion !== this) {
+    // Если есть открытый аккордеон, который не совпадает с текущим
+    openAccordion.classList.remove('active'); // закрыть его
+    const openAccordionContent = openAccordion.nextElementSibling;
+    if (openAccordionContent) {
+      // если у аккордеона есть содержимое
+      openAccordionContent.style.maxHeight = null; // сбросить высоту контента
+    }
+  }
+
+  this.classList.toggle('active'); // открыть или закрыть текущий аккордеон
+
+  const content = this.nextElementSibling;
+  if (content) {
+    // если у аккордеона есть содержимое
+    if (content.style.maxHeight) {
+      // Если контент открыт, закрыть его
+      content.style.maxHeight = null;
+    } else {
+      // Если контент закрыт, открыть его
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  }
+
+  openAccordion = this; // запомнить ссылку на открытый аккордеон
+}
+
+accordionItems.forEach(item => item.addEventListener('click', toggleAccordion));
+
